697711d17c2942f0e5ad916feeeeb99effa71976
Stanley Egbe <57241303+Stan081@users.noreply.github.com>
2025-01-15 13:36:35 +0000

    Botto navigator update and appbar additiion to base app component

--- lib/components/general_components.dart
+++ lib/components/general_components.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:lea_pay/components/appbar.dart';
+import 'package:lea_pay/screens/components.dart';
 import 'package:lea_pay/screens/dashboard_screen.dart';
 import 'package:lea_pay/screens/make_payment_screen.dart';
+import 'package:lea_pay/utils/contants.dart';
 
 class BaseAppComponent extends StatelessWidget {
   final Widget body;
+  final double? paddingTop = 16;
   const BaseAppComponent({super.key, required this.body});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color(0xffF3F3F3),
+        backgroundColor: backgroundColor,
+        appBar: const MyAppBar(),
         body: Padding(
-          padding: const EdgeInsets.all(16),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: SafeArea(child: body),
         ),
         bottomNavigationBar: const BottomNavigation());
@@ -29,7 +34,10 @@ class BottomNavigation extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: 90,
-      decoration: const BoxDecoration(color: Colors.white),
+      decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20), topRight: Radius.circular(20))),
       child: Padding(
         padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
         child: Row(
@@ -82,13 +90,49 @@ class BottomNavigation extends StatelessWidget {
               ),
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const Components(),
+                  ),
+                );
+              },
               icon: SvgPicture.asset(
                 'assets/icons/5.svg',
                 color: const Color(0xff004D43),
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class PrimaryButton extends StatelessWidget {
+  final String text;
+  final Function action;
+  const PrimaryButton({super.key, required this.text, required this.action});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 63,
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {
+          action();
+        },
+        style: ElevatedButton.styleFrom(
+            backgroundColor: secondaryColor,
+            shadowColor: Colors.transparent,
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(30))),
+            elevation: 20),
+        child: Text(
+          text,
+          style: const TextStyle(color: Color(0xff004D43), fontSize: 15),
         ),
       ),
     );
--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -14,165 +14,141 @@ class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
     return BaseAppComponent(
-        body: SafeArea(
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                CircleAvatar(
-                  child: IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.person_2_outlined)),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(children: [
+                SizedBox(
+                  width: double.infinity,
+                  height: 160,
+                  child: Image.asset(
+                    'assets/illustrations/balance_banner.jpg',
+                    fit: BoxFit.fill,
+                  ),
                 ),
-                const Text(
-                  "Welcome Lea",
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                ),
-                const Stack(
-                  children: [
-                    Icon(
-                      Icons.notifications_none_rounded,
-                      size: 35,
-                    )
-                  ],
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Stack(children: [
-              SizedBox(
-                width: double.infinity,
-                height: 160,
-                child: Image.asset(
-                  'assets/illustrations/balance_banner.jpg',
-                  fit: BoxFit.fill,
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Total Balance',
-                        style: TextStyle(fontSize: 20, color: Colors.white)),
-                    Text(
-                      "\$100,000,000",
-                      style: TextStyle(
-                          fontSize: 35,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                    ),
-                  ],
-                ),
-              ),
-              Positioned(
-                top: 90,
-                left: 240,
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const WalletScreen()));
-                  },
-                  child: const Row(
+                const Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text('My Wallet '),
-                      Icon(
-                        Icons.arrow_circle_right,
-                        size: 45,
+                      Text('Total Balance',
+                          style: TextStyle(fontSize: 20, color: Colors.white)),
+                      Text(
+                        "\$100,000,000",
+                        style: TextStyle(
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
                       ),
                     ],
                   ),
                 ),
-              )
-            ]),
-            const SizedBox(
-              height: 30,
-            ),
-            const Headings(text: 'Quick Actions'),
-            const SizedBox(
-              height: 5,
-            ),
-            const SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  QuickActionCards(
-                    icon: 'assets/icons/recharge.svg',
-                    time: 'Bills',
-                  ),
-                  QuickActionCards(
-                    icon: 'assets/icons/charity.svg',
-                    time: 'Charity',
-                  ),
-                  QuickActionCards(
-                    icon: 'assets/icons/loan.svg',
-                    time: 'Loan',
-                  ),
-                  QuickActionCards(
-                    icon: 'assets/icons/gift.svg',
-                    time: 'Gifts',
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Headings(text: 'Scheduled Payment'),
-                TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'View All',
-                    style: TextStyle(color: Color(0xffA4A9AE), fontSize: 16),
+                Positioned(
+                  top: 90,
+                  left: 220,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const WalletScreen()));
+                    },
+                    child: const Row(
+                      children: [
+                        Text('My Wallet '),
+                        Icon(
+                          Icons.arrow_circle_right,
+                          size: 45,
+                        ),
+                      ],
+                    ),
                   ),
                 )
-              ],
-            ),
-            const Padding(
-              padding: EdgeInsets.only(bottom: 60.0),
-              child: Column(
+              ]),
+              const SizedBox(
+                height: 30,
+              ),
+              const Headings(text: 'Quick Actions'),
+              const SizedBox(
+                height: 5,
+              ),
+              const SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    QuickActionCards(
+                      icon: 'assets/icons/recharge.svg',
+                      time: 'Bills',
+                    ),
+                    QuickActionCards(
+                      icon: 'assets/icons/charity.svg',
+                      time: 'Charity',
+                    ),
+                    QuickActionCards(
+                      icon: 'assets/icons/loan.svg',
+                      time: 'Loan',
+                    ),
+                    QuickActionCards(
+                      icon: 'assets/icons/gift.svg',
+                      time: 'Gifts',
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  PaymentsCards(
-                    vendor: 'Netflix',
-                    logo: 'assets/icons/netflix.png',
-                    price: '10.00',
-                    time: '11:00',
-                    points: '25',
-                  ),
-                  PaymentsCards(
-                    vendor: 'Paypal',
-                    logo: 'assets/icons/paypal.png',
-                    price: '200.00',
-                    time: '14:35',
-                    points: '10',
-                  ),
-                  PaymentsCards(
-                    vendor: 'Spotify',
-                    logo: 'assets/icons/spotify.png',
-                    price: '23.85',
-                    time: '19:47',
-                    points: null,
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
+                  const Headings(text: 'Scheduled Payment'),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'View All',
+                      style: TextStyle(color: Color(0xffA4A9AE), fontSize: 16),
+                    ),
+                  )
                 ],
               ),
-            )
-          ],
+              const Padding(
+                padding: EdgeInsets.only(bottom: 60.0),
+                child: Column(
+                  children: [
+                    PaymentsCards(
+                      vendor: 'Netflix',
+                      logo: 'assets/icons/netflix.png',
+                      price: '10.00',
+                      time: '11:00 AM',
+                      points: '25',
+                    ),
+                    PaymentsCards(
+                      vendor: 'Paypal',
+                      logo: 'assets/icons/paypal.png',
+                      price: '200.00',
+                      time: '02:35 PM',
+                      points: '10',
+                    ),
+                    PaymentsCards(
+                      vendor: 'Spotify',
+                      logo: 'assets/icons/spotify.png',
+                      price: '23.85',
+                      time: '07:47 PM',
+                      points: null,
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
 
