42dc7b701e824e639cead6f9356f3c2be3449e4a
Stanley Egbe <57241303+Stan081@users.noreply.github.com>
2025-01-15 13:38:50 +0000

    Added transaction and transaction details screens

---
+++ lib/screens/transaction_details_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TransactionDetailsScreen extends StatefulWidget {
+  const TransactionDetailsScreen({super.key});
+
+  @override
+  State<TransactionDetailsScreen> createState() =>
+      _TransactionDetailsScreenState();
+}
+
+class _TransactionDetailsScreenState extends State<TransactionDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
