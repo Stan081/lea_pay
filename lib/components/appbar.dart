ae472d625abb4c39e1387b16a8cb2f9d21084302
Stanley Egbe <57241303+Stan081@users.noreply.github.com>
2025-01-17 17:55:30 +0000

    Implemented seperate App bars and made it reuseable

--- lib/components/appbar.dart
+++ lib/components/appbar.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:lea_pay/components/buttons.dart';
 import 'package:lea_pay/utils/contants.dart';
 
-class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const MyAppBar({
+class WelcomeAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const WelcomeAppBar({
     super.key,
   });
 
@@ -10,14 +11,14 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Container(
           color: backgroundColor,
-          child: const Row(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Column(
+              const Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -27,18 +28,65 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
                   ),
                   Text(
                     "Welcome to LeaPay",
-                    style: TextStyle(fontSize: 17),
+                    style: TextStyle(fontSize: 18),
                   ),
                 ],
               ),
               Stack(
                 children: [
-                  Icon(
-                    Icons.notifications_none_rounded,
-                    size: 35,
+                  Container(
+                    width: 45,
+                    height: 45,
+                    decoration: BoxDecoration(
+                        border: Border.all(),
+                        borderRadius: BorderRadius.circular(12)),
+                    child: const Icon(
+                      Icons.notifications_none_rounded,
+                      size: 25,
+                    ),
                   )
                 ],
               )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size(double.infinity, 80);
+}
+
+class NavigatorAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const NavigatorAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Container(
+          color: backgroundColor,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              PrimaryIconButton(
+                icon: const Icon(
+                  Icons.arrow_back_ios_outlined,
+                  color: Colors.black,
+                  size: 18,
+                ),
+                action: () {},
+              ),
+              const Text(
+                'Transactions',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              const NotificationButton()
             ],
           ),
         ),
