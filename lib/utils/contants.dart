b4a31d085cb625e29d0206f94b1d067209f0054b
Stanley Egbe <57241303+Stan081@users.noreply.github.com>
2025-01-17 17:58:30 +0000

    Adjusted app background color

--- lib/utils/contants.dart
+++ lib/utils/contants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-Color backgroundColor = const Color.fromARGB(255, 241, 240, 240);
+Color backgroundColor = const Color.fromARGB(255, 244, 244, 244);
 Color primaryColor = const Color(0xff004D43);
 Color secondaryColor = const Color(0xffD0FF71);
