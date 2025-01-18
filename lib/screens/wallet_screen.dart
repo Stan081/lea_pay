5ce356c0324c86311b196de3fd9e6730df9600a2
Stanley Egbe <57241303+Stan081@users.noreply.github.com>
2025-01-18 10:50:42 +0000

    Added transactions to wallet

--- lib/screens/wallet_screen.dart
+++ lib/screens/wallet_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:lea_pay/components/appbar.dart';
 import 'package:lea_pay/components/dashboard_components.dart';
 import 'package:lea_pay/components/general_components.dart';
+import 'package:lea_pay/components/transactions.dart';
 import 'package:lea_pay/components/virtual_card.dart';
 
 class WalletScreen extends StatefulWidget {
@@ -16,17 +17,140 @@ class _WalletScreenState extends State<WalletScreen> {
   @override
   Widget build(BuildContext context) {
     return BaseAppComponent(
-      appBar: const NavigatorAppBar(),
+      appBar: const NavigatorAppBar(
+        header: 'Wallet',
+      ),
       body: SingleChildScrollView(
         child: Column(
           children: [
             const SingleChildScrollView(
               dragStartBehavior: DragStartBehavior.start,
               scrollDirection: Axis.horizontal,
-              child: Row(
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 20),
+                child: Row(
+                  children: [
+                    VirtualCard(),
+                    VirtualCard(),
+                  ],
+                ),
+              ),
+            ),
+            TransactionCard(
+              heading: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  VirtualCard(),
-                  VirtualCard(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Date',
+                        style: TextStyle(
+                            color: Colors.grey[400],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16),
+                      ),
+                      Text(
+                        'Total',
+                        style: TextStyle(
+                            color: Colors.grey[400],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16),
+                      ),
+                    ],
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Headings(text: 'October 17, Thu'),
+                      Headings(text: '\$234.80'),
+                    ],
+                  ),
+                ],
+              ),
+              content: const Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TransactionItem(
+                    vendor: 'Netflix',
+                    logo: 'assets/icons/netflix.png',
+                    price: '10.00',
+                    time: '11:00 AM',
+                    points: '25',
+                  ),
+                  TransactionItem(
+                    vendor: 'Paypal',
+                    logo: 'assets/icons/paypal.png',
+                    price: '200.00',
+                    time: '02:35 PM',
+                    points: '10',
+                  ),
+                  TransactionItem(
+                    vendor: 'Spotify',
+                    logo: 'assets/icons/spotify.png',
+                    price: '23.85',
+                    time: '07:47 PM',
+                    points: '0',
+                  ),
+                  TransactionItem(
+                    vendor: 'Spotify',
+                    logo: 'assets/icons/spotify.png',
+                    price: '23.85',
+                    time: '07:47 PM',
+                    points: '0',
+                  ),
+                ],
+              ),
+            ),
+            TransactionCard(
+              heading: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Date',
+                        style: TextStyle(
+                            color: Colors.grey[400],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16),
+                      ),
+                      Text(
+                        'Total',
+                        style: TextStyle(
+                            color: Colors.grey[400],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16),
+                      ),
+                    ],
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Headings(text: 'October 20, Sun'),
+                      Headings(text: '\$35.80'),
+                    ],
+                  ),
+                ],
+              ),
+              content: const Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TransactionItem(
+                    vendor: 'Netflix',
+                    logo: 'assets/icons/netflix.png',
+                    price: '10.00',
+                    time: '11:00 AM',
+                    points: '25',
+                  ),
+                  TransactionItem(
+                    vendor: 'Spotify',
+                    logo: 'assets/icons/spotify.png',
+                    price: '23.85',
+                    time: '07:47 PM',
+                    points: '0',
+                  ),
                 ],
               ),
             ),
