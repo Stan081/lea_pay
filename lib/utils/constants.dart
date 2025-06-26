b0dd9ddb6b031c3ee3f4d6b25ec2f29c6700ace0
Stanley Egbe <57241303+Stan081@users.noreply.github.com>
2025-06-26 01:35:27 +0100

    update: updated project conatnts to be more detailed and comprehensive

---
+++ lib/utils/constants.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+// Font Sizes
+const double kFontSizeExtraSmall = 12.0;
+const double kFontSizeSmall = 16.0;
+const double kFontSizeMedium = 20.0;
+const double kFontSizeLarge = 24.0;
+const double kFontSizeXLarge = 28.0;
+const double kKeypadFontSize = 32.0;
+const double kCardHeight = 220.0;
+const double kAmountFontSize = 48.0;
+const double kFontSizeExtraLarge = 40.0;
+const double kFontSizeXXLarge = 34.0;
+
+// Spacing
+const double kSpacingExtraSmall = 10.0;
+const double kSpacingTiny = 4.0;
+const double kSpacingSmall = 12.0;
+const double kSpacingMedium = 16.0;
+const double kSpacingLarge = 20.0;
+const double kSpacingXLarge = 32.0;
+const double kSpacingXXLarge = 40.0;
+const double kDefaultPadding = 20.0;
+const double kSpacingXXXLarge = 50.0;
+
+// Spacing
+const double kSpacingHuge = 40.0;
+
+// Border Radius
+const double kRadiusSmall = 4.0;
+const double kRadiusMedium = 16.0;
+const double kRadiusDefault = 12.0;
+const double kRadiusXLarge = 20.0;
+const double kRadiusXXLarge = 30.0;
+const double kRadiusXXXLarge = 40.0;
+const double kRadiusCircle = 110.0;
+const double kButtonRadius = 30.0;
+
+// Colors
+
+const Color kPrimaryColor = Color(0xFF004D43);
+const Color backgroundColor = Color(0xFFF9F9F9);
+const Color kSuccessColor = Colors.green;
+const Color kErrorColor = Colors.red;
+const Color kSecondaryColor = Color(0xFFD0FF71);
+const Color kButtonTextColor = Color(0xFF004D43);
+const Color kBorderColor = Color(0xFFE0E0E0);
+const Color kLightGreyColor = Color.fromARGB(255, 204, 200, 200);
+const Color kGreyColor = Color.fromARGB(255, 242, 242, 242);
+const Color kDarkGreyColor = Color.fromARGB(255, 37, 36, 36);
+
+// Icon Sizes
+const double kIconSizeSmall = 18.0;
+const double kIconSizeMedium = 24.0;
+const double kIconSizeLarge = 40.0;
+const double kKeypadIconSize = 28.0;
+const double kIconSizeHuge = 120.0;
+const double kIconButtonSize = 45.0;
+const double kButtonHeight = 56.0;
+const double kBorderWidth = 2.0;
+
+// Image Dimensions
+const double kImageWidth = 200.0;
+const double kImageHeight = 200.0;
+const double kLogoSize = 60.0;
+const double kAppBarHeight = 60.0;
+const double kCircularProgressSize = 20.0;
+const double kCircularProgressStrokeWidth = 2.0;
